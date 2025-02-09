b7e4f2a2e3f87bfc4b24baf9a131623a4b7566f0
fernandolf88 <fernandolf@alumni.usp.br>
2025-02-09 20:20:03 -0300

    Iniciando o projeto

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pygame
+import tkinter as tk
+
+pygame.init()
+screen = pygame.display.set_mode((1200,720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    screen .fill('black')
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
